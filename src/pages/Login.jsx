dfabacd379af702310f699ece9007a57eca28a6f
63nitin <yadavnitin638675@bbdnitm.ac.in>
2024-11-07 12:27:01 +0530

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Login as loginComponet} from '../components'
+function Login() {
+    return (
+        <div className="py-8">
+            <loginComponet/>
+        </div>
+    )
+}
+
+export default Login
